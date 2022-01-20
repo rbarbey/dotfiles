1aa03c34b9a8c06f791c90b46486fbfbf74172b4
Robert Barbey <robert.barbey@gmail.com>
2022-01-20 22:58:16 +0100

    Installed more packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -37,7 +37,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(restclient restclient-test markdown-mode gitlab-ci-mode magit)))
+   '(flycheck-golangci-lint flycheck-projectile go-mode go-projectile restclient restclient-test markdown-mode gitlab-ci-mode magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

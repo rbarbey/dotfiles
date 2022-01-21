198cc1c03b78bc4ed6af2887563a0228a1ef2995
Robert Barbey <robert.barbey@gmail.com>
2022-01-21 20:56:04 +0100

    Added Helm

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -36,13 +36,20 @@
 
 (require 'git-commit)
 
+;; Helm
+(require 'helm)
+(require 'helm-config)
+(require 'helm-projectile)
+(helm-mode 1)
+(helm-projectile-on)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(flycheck-golangci-lint flycheck-projectile go-mode go-projectile restclient restclient-test markdown-mode gitlab-ci-mode magit)))
+   '(helm helm-projectile flycheck-golangci-lint flycheck-projectile go-mode go-projectile restclient restclient-test markdown-mode gitlab-ci-mode magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

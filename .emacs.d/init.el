69d6b8379d795f4486cfe22d5c5264b0d31ed382
Robert Barbey <robert.barbey@gmail.com>
2022-01-23 07:45:16 +0100

    Enable auto-fill-mode for Markdown, wrap at 80

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -60,12 +60,18 @@
 
 ;; Adjust colors
 (custom-set-faces
- '(font-lock-function-name-face ((t (:bold t :foreground "#0123f5"))))
-)
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-function-name-face ((t (:bold t :foreground "#0123f5")))))
 
 ;; Markdown mode
 (add-hook 'markdown-mode-hook
- (lambda () (setq whitespace-line-column 80)))
+	  (lambda ()
+	    (auto-fill-mode 1)
+	    (setq whitespace-line-column 80)
+	    (setq fill-column 80)))
 
 
 (custom-set-variables

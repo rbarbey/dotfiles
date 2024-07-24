87f2e6e0506ce8af8922b64c0e611a7be6d12a37
Robert Barbey <robert.barbey@gmail.com>
2024-07-24 09:17:55 +0200

    [emacs] added function that deletes current buffer's file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -425,5 +425,19 @@ The string returns the filename where to store archived tasks. It
 ;; MMorph programming
 (add-to-list 'auto-coding-alist
              '("\\.mmo\\(rph\\)?$" . latin-9))
+(defun rb/delete-current-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if filename
+        (if (y-or-n-p (concat "Do you really want to delete file"
+                              filename
+                              "?"))
+            (progn
+              (delete-file filename)
+              (message "Deleted file %s." filename)
+              (kill-buffer)))
+      (message "Not a file visiting buffer"))))
+
 
 ;;; init.el ends here

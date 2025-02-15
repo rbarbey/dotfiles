de10823a184be7c16edda30c925dd7d2896cb6af
Robert Barbey <robert.barbey@acrolinx.com>
2025-02-15 12:00:35 +0100

    [emacs] formatting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -46,9 +46,10 @@
 (require 'package)
 
 ;; Add package repositories
-(setq package-archives'(("melpa" . "https://melpa.org/packages/")
-                         ("org" .  "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("org"   .  "https://orgmode.org/elpa/")
+        ("elpa"  . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 (unless package-archive-contents
